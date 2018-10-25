cf7a652ceb228a482b01565b98511308ff24ce72
josh1351 <joshua.rios@authentix.com>
2018-10-25 15:21:42 -0500

    Update HyperLink.java

--- src/main/java/com/ea/framework/controls/elements/HyperLink.java
+++ src/main/java/com/ea/framework/controls/elements/HyperLink.java
@@ -3,9 +3,6 @@ package com.ea.framework.controls.elements;
 import com.ea.framework.controls.api.ImplementedBy;
 import com.ea.framework.controls.internals.Control;
 
-/**
- * Created by Karthik-pc on 12/10/2016.
- */
 
 @ImplementedBy(HyperLinkBase.class)
 public interface HyperLink extends Control{
